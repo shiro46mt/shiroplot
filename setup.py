d889c405824fe7ce4ee3f1ca6075500e1209305e
shiro <39456646+shiro46mt@users.noreply.github.com>
2022-12-31 16:33:47 +0900

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shiroplot',
-    version='1.0',
+    version='1.0.1',
     install_requires=['matplotlib'],
     packages=find_packages()
 )
