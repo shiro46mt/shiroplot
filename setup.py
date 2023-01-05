a67c0c0d93c9f19bdab57e7b8b046392b997d63a
SHIRO <39456646+shiro46mt@users.noreply.github.com>
2023-01-05 22:12:36 +0900

    v1.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='shiroplot',
-    version='1.1.0',
+    version='1.1.1',
+    author='SHIRO',
+    author_email='39456646+shiro46mt@users.noreply.github.com',
+    license='MIT',
+    description='Shiroplot is a Python visualization library inspired by seaborn.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['matplotlib'],
     packages=find_packages()
 )
